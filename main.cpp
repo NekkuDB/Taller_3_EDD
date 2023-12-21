2a1fe703e203c8b078437a1643d12405aadc4a88
D-FeriB <91104454+D-FeriB@users.noreply.github.com>
2023-12-21 02:54:31 -0400

    Added Functionality

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@
 #include <unordered_map>
 #include <list>
 #include <limits>
+#include <cctype>
 
 struct Nodo {
+  int id;
   std::string nombre;
-  std::string ip;
-  // Otros atributos...
+  std::string tipo;
 };
 
 struct Arista {
   Nodo* origen;
   Nodo* destino;
-  int peso; // Ejemplo: tiempo de envío
+  int velocidad;
+  int distancia;
 };
 
 int main() {
@@ -23,11 +25,28 @@ int main() {
   std::unordered_map<std::string, Nodo*> nodoMap;
   std::vector<Arista> aristas;
 
+    // ... [resto del código inicial]
+
+    int pesoArchivo, numeroParticiones;
+    std::cout << "Ingrese el peso del archivo en MB: ";
+    std::cin >> pesoArchivo;
+
+    numeroParticiones = (pesoArchivo + 299) / 300; // Asume que la capacidad máxima por conexión es de 300 MB
+    std::cout << "Número de particiones necesarias: " << numeroParticiones << std::endl;
+
+    std::string nombreDestino;
+    std::cout << "Ingrese el nombre del nodo destino: ";
+    std::cin >> nombreDestino;
+    Nodo* nodoDestino = nodoMap[nombreDestino]; // Asegúrate de que este nodo existe en tu mapa
+
+
+
+
   CSVReader readerServidores("servidores.csv");
   auto datosServidores = readerServidores.readData();
 
   for (auto& fila : datosServidores) {
-    Nodo nodo = {fila[0], fila[1]}; // Asume que fila[0] es el nombre y fila[1] es la IP
+  Nodo nodo = {std::stoi(fila[0]), fila[1], fila[2]};
     nodos.push_back(nodo);
     nodoMap[nodo.nombre] = &nodos.back(); // Mapeo del nombre al Nodo
   }
@@ -36,17 +55,16 @@ int main() {
   auto datosConexiones = readerConexiones.readData();
 
   for (auto& fila : datosConexiones) {
-    Nodo* origen = nodoMap[fila[0]]; // Asume que fila[0] es el nombre del nodo de origen
-    Nodo* destino = nodoMap[fila[1]]; // Asume que fila[1] es el nombre del nodo de destino
-    int peso = std::stoi(fila[2]); // Asume que fila[2] es el peso de la arista
-
-    Arista arista = {origen, destino, peso};
-    aristas.push_back(arista);
+      Nodo* origen = nodoMap[fila[0]];
+      Nodo* destino = nodoMap[fila[1]];
+      int velocidad = std::stoi(fila[2]);
+      int distancia = std::stoi(fila[3]);
+    
+      Arista arista = {origen, destino, velocidad, distancia};
   }
 
   // Grafo representado como lista de adyacencia
   std::unordered_map<Nodo*, std::list<Arista>> grafo;
-
   for (const auto& arista : aristas) {
     Nodo* origen = arista.origen;
     grafo[origen].push_back(arista);
@@ -59,30 +77,24 @@ int main() {
     nodoIndices[&nodo] = index++;
   }
 
+  std::unordered_map<Nodo*, Nodo*> predecesores;
+  
   // Inicialización
   std::vector<int> distancias(nodos.size(), std::numeric_limits<int>::max());
   distancias[nodoIndices[&nodos[0]]] = 0; // Inicializa la distancia del primer nodo a 0
-  
-    int pesoArchivo;
-    std::cout << "Ingrese el peso del archivo en MB: ";
-    std::cin >> pesoArchivo;
-
-    int numeroParticiones = pesoArchivo / 300; // Asumiendo que 300 MB es el máximo por conexión
-    if (pesoArchivo % 300 != 0) {
-        numeroParticiones++; // Añadir una partición extra para el residuo
-    }
+ 
 
   // Iteración sobre las aristas
   for (int iteracion = 0; iteracion < grafo.size() - 1; iteracion++) {
     for (const auto& arista : aristas) {
-      Nodo* u = arista.origen;
-      Nodo* v = arista.destino;
-      int peso = arista.peso;
+        Nodo* u = arista.origen;
+        Nodo* v = arista.destino;
+        int tiempoConexion = arista.distancia * numeroParticiones; // Tiempo total considerando las particiones
 
-      // Relajación
-      if (distancias[nodoIndices[u]] != std::numeric_limits<int>::max() && distancias[nodoIndices[u]] + peso < distancias[nodoIndices[v]]) {
-        distancias[nodoIndices[v]] = distancias[nodoIndices[u]] + peso;
-      }
+        if (distancias[nodoIndices[u]] + tiempoConexion < distancias[nodoIndices[v]]) {
+            distancias[nodoIndices[v]] = distancias[nodoIndices[u]] + tiempoConexion;
+            predecesores[v] = u;
+        }
     }
   }
 
@@ -90,7 +102,7 @@ int main() {
   for (const auto& arista : aristas) {
     Nodo* u = arista.origen;
     Nodo* v = arista.destino;
-    int peso = arista.peso;
+    int peso = arista.velocidad;
 
     if (distancias[nodoIndices[u]] != std::numeric_limits<int>::max() && distancias[nodoIndices[u]] + peso < distancias[nodoIndices[v]]) {
       // Se encontró un ciclo negativo
@@ -98,5 +110,26 @@ int main() {
       return 0;
     }
   }
+  Nodo* actual = nodoDestino; // Asumiendo que tienes un nodoDestino
+  while (actual != nullptr) {
+      std::cout << "Nodo: " << actual->nombre << " - Tiempo hasta ahora: " << distancias[nodoIndices[actual]] << " segundos" << std::endl;
+      actual = predecesores[actual];
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
